edfda927a7407a471a1180f9dd452c363740efec
Keith <4mythings@gmail.com>
2019-10-29 22:43:30 -0600

    Lesson 1

---
+++ src/home.comp.js
@@ -0,0 +1,47 @@
+import React from "react"
+import "./homepage.scss"
+
+const HomePage = () => (
+  <div className="homepage">
+    <h1>Welcome to my Site</h1>
+
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">HATS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage
