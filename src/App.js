004e03149119bba362ee12cb30602711a2682e67
Keith <4mythings@gmail.com>
2019-10-30 21:53:44 -0600

    Lesson 71 Header Component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css"
 
 import HomePage from "./pages/homepage/homepage.comp"
 import ShopPage from "./pages/shop/shop.comp"
+import Header from "./components/header/header-comp"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
