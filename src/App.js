18daab4803f650cf061512f2b383f68d401c5af1
Keith <4mythings@gmail.com>
2019-10-30 21:24:31 -0600

    Lesson 70 Collection Item

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Switch, Route } from "react-router-dom"
 import "./App.css"
 
 import HomePage from "./pages/homepage/homepage.comp"
-
-const HatsPage = () => (
-  <div>
-    <h1>My Hats</h1>
-  </div>
-)
+import ShopPage from "./pages/shop/shop.comp"
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   )
---
+++ src/components/collection-item/collection-item.comp.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import "./collection-item.scss"
+
+import CollectionPreview from "../collection-preview/collection-preview.comp"
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+)
+
+export default CollectionItem
